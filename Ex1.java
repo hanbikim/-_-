8642c445c79f29cf9c05b6a953a3aafb9b7072fc
Hanbi-Kim <74577775+hanbikim@users.noreply.github.com>
2021-09-13 12:33:54 +0900

    Add files via upload
    2장 예제

---
+++ Ex1.java
@@ -0,0 +1,19 @@
+//
+public class Ex1 {
+    public static int sum(int n, int m){
+        return n+m;
+    }
+
+    public static void main(String[] args){
+        int i=20;
+        int s;
+        char a;
+
+        s=sum(i,20); //40
+        a='?';
+        System.out.println(a); //?
+        System.out.println("Hello World"); //Hello World
+        System.out.println(s); //40
+    }
+}
+
