03ab71eb2e5bd31cb4ce9c29abce1695e3f1a6fb
Hanbi-Kim <74577775+hanbikim@users.noreply.github.com>
2021-10-03 19:18:49 +0900

    Add files via upload
    파일 정리

---
+++ 2장/예제/practice1.java
@@ -0,0 +1,17 @@
+public class practice1 {
+    public static int sum(int n, int m){
+        return n+m;
+    }
+
+    public static void main(String[] args){
+        int i=20;
+        int s;
+        char a;
+
+        s=sum(i,20); //40
+        a='?';
+        System.out.println(a); //?
+        System.out.println("Hello World"); //Hello World
+        System.out.println(s); //40
+    }
+}
