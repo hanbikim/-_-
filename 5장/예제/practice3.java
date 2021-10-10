369ef80f414de4c59dce409bd1cb8684912fbb84
Hanbi-Kim <74577775+hanbikim@users.noreply.github.com>
2021-10-10 14:05:40 +0900

    Add files via upload

---
+++ 5장/예제/practice3.java
@@ -0,0 +1,31 @@
+class Point3 {
+    private int x, y;
+    public Point3() {
+        this.x = this.y = 0;
+    }
+    public Point3(int x, int y) {
+        this.x = x; this.y = y;
+    }
+    public void showPoint() {
+        System.out.println("(" + x + "," + y + ")");
+    }
+}
+
+class ColorPoint3 extends Point3 {
+    private String color;
+    public ColorPoint3(int x, int y, String color) {
+        super(x, y);
+        this.color = color;
+    }
+    public void showColorPoint() {
+        System.out.print(color);
+        showPoint();
+    }
+}
+
+public class practice3 {
+    public static void main(String[] args) {
+        ColorPoint3 cp = new ColorPoint3(5, 6, "blue");
+        cp.showColorPoint();
+    }
+}
